fd3e30a6af40dec4f5b614721f34897bf432682f
TravelFiend <mikegrace274@gmail.com>
2019-10-01 12:12:19 -0700

    added a bit more functionality

--- src/app.js
+++ src/app.js
@@ -1,25 +1,41 @@
 import { isYes } from './is-yes.js';
 const myButton = document.getElementById('quiz-button');
+const myQuizResults = document.getElementById('quizResults');
+const myScore = document.getElementById('score');
+let total = 0;
+let percent
 
 myButton.addEventListener('click', () => {
     alert('Welcome to my quiz!');
     
     const userName = prompt('What is your name?');
-    const myConfirmation = confirm('Do you want to take my quiz?');
+    const myConfirmation = confirm(`Hey ${userName}, do you want to take my quiz?`);
 
-    if(myConfirmation){ 
-        alert(`Let's get this party started!`);
+    if (myConfirmation){ 
+
         const answer1 = prompt('Have I visited Portugal?');
+        const sanitizedUserAnswer1 = answer1.toLowerCase().trim();
+        const userTypedYes1 = isYes(sanitizedUserAnswer1);
+        if (!userTypedYes1){
+            total++;
+        }
+
         const answer2 = prompt(`Do I have a bachelor's degree in printmaking?`);
+        const sanitizedUserAnswer2 = answer2.toLowerCase().trim();
+        const userTypedYes2 = isYes(sanitizedUserAnswer2);
+        if (userTypedYes2) {
+            total++;
+        }
+
         const answer3 = prompt(`Was my first downhill ride in Wyoming?`);
-
-        
-        const lowercaseUserAnswer = userAnswer.toLowerCase();
-        const trimmedLowercaseUserAnswer = lowercaseUserAnswer.trim();
-
-        const userTypedYes = isYes(trimmedLowercaseUserAnswer);
+        const sanitizedUserAnswer3 = answer3.toLowerCase().trim();
+        const userTypedYes3 = isYes(sanitizedUserAnswer3);
+        if (!userTypedYes3) {
+            total++;
+        }
 
         alert(`Quiz complete!  Your score will now show on the webpage.`);
     }
+    myScore.innerHTML = `You completed the quiz ${userName}!  Your score is ${myScore}/3.`;
     return;
 });
